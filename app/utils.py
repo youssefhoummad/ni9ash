491be3c29b7ae57b2c264c67fe8d91acb3d6cad5
youssefhoummad <youssef.hoummad@outlook.com>
2019-07-08 01:50:48 +0100

    fix comment nested

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,7 @@ def get_object_or_none(model, *args, **kwargs):
         return model._default_manager.get(*args, **kwargs)
     except model.DoesNotExist:
         return None
+
+
+def get_object_or_this(model, this, *args, **kwargs):
+    return get_object_or_none(model, *args, **kwargs) or this
