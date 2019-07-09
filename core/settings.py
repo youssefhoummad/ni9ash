d890aa6dc24d20cc47f5671e382fa774f541a47e
youssefhoummad <youssef.hoummad@outlook.com>
2019-07-09 09:48:54 +0100

    add image profile in navbar

--- core/settings.py
+++ core/settings.py
@@ -147,6 +147,8 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
